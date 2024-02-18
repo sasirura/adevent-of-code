6cea354c60a250b6fbffa4804a32e2a347124dc5
Sasiru Ravihansa <sasiruravihansa@gmail.com>
2024-02-18 08:53:25 +0530

    solved day2 part 2

--- year2023/day2/cube.go
+++ year2023/day2/cube.go
@@ -9,6 +9,53 @@ import (
     "strings"
 )
 
+func parseLinePart2(line string) int {
+
+    if line == "" {
+        return 0
+    }
+
+    parts := strings.Split(line, ":")
+
+    colorParts := strings.Split(parts[1], ";")
+
+    power := 0
+    blue := 0
+    red := 0
+    green := 0
+
+    for _, game := range colorParts{
+
+        sets := strings.Split(game, ",") 
+        for _, set := range sets{
+
+            color := strings.Split(set, " ") 
+
+            cube, err := strconv.Atoi(color[1])
+
+            if err != nil {
+                log.Fatal("this should not happend" )
+            }
+
+            if color[2] == "red" {
+                if red < cube {
+                    red = cube
+                }
+            } else if color[2] == "blue" {
+                if blue < cube {
+                    blue = cube
+                }
+            } else if green < cube {
+                green = cube
+            }
+        }
+    }
+
+    power = green * blue * red
+
+    return power
+}
+
 
 
 func parseLine(line string) int {
@@ -65,8 +112,6 @@ func parseLine(line string) int {
     }
 
     return gameNumber
-
-
 }
 
 func main(){
@@ -87,6 +132,25 @@ func main(){
         total += current
     }
 
-    fmt.Printf("value is %d" , total)
+    fmt.Printf("value is %d\n" , total)
+
+    part2, err := os.ReadFile("day2p2.txt")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    getInput2 := string(part2)
+    lines2 :=  strings.Split(getInput2, "\n")
+
+    total2 := 0
+    for _, line := range lines2 {
+
+        current := parseLinePart2(line)
+
+        total2 += current
+    }
+
+    fmt.Printf("value is %d\n" , total2)
 }
 
